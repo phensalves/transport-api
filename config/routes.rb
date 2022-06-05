532f491a1fc8b938d8ced7ac3bc527b2fa71f8de
Pedro Alves <pedro_alves@me.com>
2022-06-05 18:02:51 -0300

    Updating routes to api/v1 path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :operations
-  resources :contacts
-  resources :facilities
-  resources :customers
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :operations
+      resources :contacts
+      resources :facilities
+      resources :customers
+      # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    end
+  end
 end
