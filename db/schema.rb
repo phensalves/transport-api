66264dac15538d717ccfc7fb4e15d67c40ab2e6f
Pedro Alves <pedro_alves@me.com>
2022-06-05 20:01:50 -0300

    creating intermediated table to has_and_belongs_to_many relationship
    With this table it is possible to generate responses with some data from both tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_05_203359) do
+ActiveRecord::Schema.define(version: 2022_06_05_225522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,15 @@ ActiveRecord::Schema.define(version: 2022_06_05_203359) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "customers_operations", force: :cascade do |t|
+    t.bigint "customer_id", null: false
+    t.bigint "operation_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["customer_id"], name: "index_customers_operations_on_customer_id"
+    t.index ["operation_id"], name: "index_customers_operations_on_operation_id"
+  end
+
   create_table "facilities", force: :cascade do |t|
     t.string "cep"
     t.string "complement"
@@ -58,5 +67,7 @@ ActiveRecord::Schema.define(version: 2022_06_05_203359) do
   end
 
   add_foreign_key "contacts", "customers"
+  add_foreign_key "customers_operations", "customers"
+  add_foreign_key "customers_operations", "operations"
   add_foreign_key "facilities", "customers"
 end
