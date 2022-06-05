a6d9cc2f6e544c2a306d024aba4013ea07a67e0b
Pedro Alves <pedro_alves@me.com>
2022-06-05 18:56:53 -0300

    Adding another relations into customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,7 @@
 class Customer < ApplicationRecord
-  has_and_belongs_to_many :operations, dependent: destroy
+  has_many :facilities, dependent: :destroy
+  has_many :operations, dependent: :destroy
+  has_many :contacts, dependent: :destroy
+
+  has_and_belongs_to_many :operations, dependent: :destroy
 end
