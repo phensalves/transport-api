0dec4de8e601210fa61c69797088873e7aab4610
Pedro Alves <pedro_alves@me.com>
2022-06-08 21:21:57 -0300

    Adding validation into facility model

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -1,3 +1,10 @@
 class Facility < ApplicationRecord
   belongs_to :customer, optional: true
+
+  validates :city, presence: true
+  validates :state, presence: { message: "State is required!" }
+  validates :cep, presence: { message: "CEP is required!" }
+  validates :country, presence: { message: "Country is required!" }
+  validates :description, presence: { message: "Description is required!" }
+  validates :number, presence: { message: "Number is required!" }
 end
