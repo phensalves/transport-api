9b640bb468266b222e92cfaebc743bf024eb5bca
Pedro Alves <pedro_alves@me.com>
2022-06-08 22:44:19 -0300

    fixing facilities create route

--- app/controllers/api/v1/facilities_controller.rb
+++ app/controllers/api/v1/facilities_controller.rb
@@ -3,9 +3,35 @@ class Api::V1::FacilitiesController < ApplicationController
 
   # GET /facilities
   def index
-    @facilities = Facility.all
+    per_page = params[:per_page] ? params[:per_page] : 10
+    @facilities = Facility.all.paginate(page: params[:page], per_page: per_page)
 
-    render json: @facilities
+    if @facilities.length >= 1
+      render json: {
+        status: 'SUCCESS',
+        menssage: 'Successfully',
+        data: @facilities,
+        per_page: per_page.to_i,
+        total_data: @facilities.count,
+        current_page: params[:page].to_i ? params[:page].to_i : 0,
+        total_pages: @facilities.total_pages
+      },
+      include: {
+        customer: { except: [:created_at, :updated_at] }
+      }
+    else
+      per_page  = 0
+      total_pages = 0
+      render json: {
+        status: 'SUCCESS',
+        menssage: 'There are no facilities registered in this page',
+        data: [],
+        per_page: per_page.to_i,
+        total_data: @facilities.count,
+        current_page: params[:page].to_i ? params[:page].to_i : 0,
+        total_pages: @facilities.total_pages
+      }
+    end
   end
 
   # GET /facilities/1
@@ -18,7 +44,15 @@ class Api::V1::FacilitiesController < ApplicationController
     @facility = Facility.new(facility_params)
 
     if @facility.save
-      render json: @facility, status: :created, location: @facility
+      render json: {
+        data:  @facility,
+        status: 'SUCCESS',
+        message: 'Saved as successfully',
+        location: api_v1_facility_url(@facility)
+      },
+      include: {
+        customer: { except: [:createad_at, :updated_at] }
+      }
     else
       render json: @facility.errors, status: :unprocessable_entity
     end
@@ -46,6 +80,14 @@ class Api::V1::FacilitiesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def facility_params
-      params.require(:facility).permit(:cep, :complement, :number, :city, :state, :country, :description, :customer_id)
+      params.require(:facility).permit(
+        :cep,
+        :complement,
+        :number,
+        :city,
+        :state,
+        :country,
+        :description,
+        :customer_id)
     end
 end
